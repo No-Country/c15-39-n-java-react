99a8a0320ef13d613e82607ffe4c120a8a5a39b2
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-11-24 16:56:19 -0500

    Se creo el proyecto de Springboot, se crearon las carpetas y estructura del proyecto, se configuro la base de datos MYSQL local

---
+++ backend/backend/src/main/java/com/nocountry/c15g39n/dto/request/UsuarioRequestDto.java
@@ -0,0 +1,35 @@
+package com.nocountry.c15g39n.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class UsuarioRequestDto {
+
+    @NotBlank(message = "El nombre es requerido")
+    @Size(min = 2, message = "El nombre debe tener al menos 2 caracteres")
+    private String nombre;
+
+    @NotBlank(message = "El apellido es requerido")
+    @Size(min = 2, message = "El apellido debe tener al menos 2 caracteres")
+    private String apellido;
+
+    @NotBlank(message = "El  documentoDeIdentidad es requerido")
+    @Pattern(regexp = "\\d+", message = "El documentoDeIdentidad debe ser númerico")
+    @Size(min = 5, message = "El documento de identidad debe tener al menos 5 numeros")
+    private String documentoDeIdentidad;
+
+    @NotBlank(message = "El correo electrónico es requerido")
+    @Email(message = "El correo electrónico debe ser válido")
+    private String correo;
+
+    @NotBlank(message = "La clave es requerida")
+    private String clave;
+
+}
---
+++ backend/backend/src/main/java/com/nocountry/c15g39n/services/IUsuarioService.java
@@ -0,0 +1,8 @@
+package com.nocountry.c15g39n.services;
+
+
+public interface IUsuarioService {
+
+
+
+}
