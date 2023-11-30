ab1d94630c0d73e46305753c2c452df54b6698d3
Belen Taborda <mbelen.taborda41@gmail.com>
2023-11-29 23:32:20 -0300

    Se implemento la logica del registro

--- backend/src/main/java/com/nocountry/c15g39n/controllers/UsuarioRestController.java
+++ backend/src/main/java/com/nocountry/c15g39n/controllers/UsuarioRestController.java
@@ -1,9 +1,13 @@
 package com.nocountry.c15g39n.controllers;
 
 
+import com.nocountry.c15g39n.dto.request.UsuarioRequestDto;
+import com.nocountry.c15g39n.dto.response.UsuarioResponseDto;
+import com.nocountry.c15g39n.services.IUsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +19,9 @@ import javax.validation.Valid;
 @RequestMapping("api/v1/usuario")
 @RequiredArgsConstructor
 public class UsuarioRestController {
-
-
-
+    private final IUsuarioService usuarioService;
+    @PostMapping("/registrar")
+    public ResponseEntity<UsuarioResponseDto> registrarAprendiz(@Valid @RequestBody UsuarioRequestDto aprendiz) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.registrarUsuario(aprendiz));
+    }
 }
--- backend/src/main/java/com/nocountry/c15g39n/services/impl/UsuarioServiceImpl.java
+++ backend/src/main/java/com/nocountry/c15g39n/services/impl/UsuarioServiceImpl.java
@@ -1,15 +1,52 @@
 package com.nocountry.c15g39n.services.impl;
 
+import com.nocountry.c15g39n.dto.request.UsuarioRequestDto;
+import com.nocountry.c15g39n.dto.response.UsuarioResponseDto;
+import com.nocountry.c15g39n.entities.Rol;
+import com.nocountry.c15g39n.entities.Usuario;
+import com.nocountry.c15g39n.mapper.UsuarioRequestToUsuario;
+import com.nocountry.c15g39n.mapper.UsuarioToReponseDto;
+import com.nocountry.c15g39n.repositories.RolRepository;
+import com.nocountry.c15g39n.repositories.UsuarioRepository;
 import com.nocountry.c15g39n.services.IUsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.security.PrivateKey;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class UsuarioServiceImpl implements IUsuarioService {
+    private final UsuarioToReponseDto usuarioToReponseDto;
+    private final UsuarioRequestToUsuario usuarioRequestToUsuario;
+    private final UsuarioRepository usuarioRepository;
+    private final RolRepository rolRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Override
+    public UsuarioResponseDto registrarUsuario(UsuarioRequestDto usuarioRequestDto) {
+      /*  UsuarioResponseDto usuarioResponseDto = new UsuarioResponseDto();*/
 
+        Usuario usuario = new Usuario();
+        usuario = usuarioRequestToUsuario.toUsuario(usuarioRequestDto);
+        usuario.setFechaRegistro(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()));
+        usuario.setClave(passwordEncoder.encode(usuarioRequestDto.getClave()));
+
+        Rol rolAprendiz = new Rol();
+        rolAprendiz = rolRepository.findById(1L).orElse(null);
+        usuario.setRol(rolAprendiz);
+        usuarioRepository.save(usuario);
+
+
+        return usuarioToReponseDto.toRespondeDto(usuario);
 
 }
+
+}
+
