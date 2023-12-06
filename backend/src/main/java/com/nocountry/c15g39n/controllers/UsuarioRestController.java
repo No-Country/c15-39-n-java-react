358ce26e5973cbd0edd53d1cd242cd94e00c25c9
Cindy Mendoza <mendozaic17@gmail.com>
2023-12-06 17:08:46 -0500

    add get data from user table

--- backend/src/main/java/com/nocountry/c15g39n/controllers/UsuarioRestController.java
+++ backend/src/main/java/com/nocountry/c15g39n/controllers/UsuarioRestController.java
@@ -3,16 +3,16 @@ package com.nocountry.c15g39n.controllers;
 
 import com.nocountry.c15g39n.dto.request.UsuarioRequestDto;
 import com.nocountry.c15g39n.dto.response.UsuarioResponseDto;
+import com.nocountry.c15g39n.entities.Usuario;
 import com.nocountry.c15g39n.services.IUsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/usuario")
@@ -22,5 +22,10 @@ public class UsuarioRestController {
     @PostMapping("/registrar")
     public ResponseEntity<UsuarioResponseDto> registrarAprendiz(@Valid @RequestBody UsuarioRequestDto aprendiz) {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.registrarUsuario(aprendiz));
+    }
+    @GetMapping("/obtener")
+    @PreAuthorize("hasAuthority('APRENDIZ')")
+    public ResponseEntity<List<Usuario>> obtenerTodosLosUsuarios(){
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioService.obtenerUsuarios());
     }
 }
