9b1e23bdd92de7e3fa92ef2a7585169b8c039f6f
SofiDevO <angiemeow89@gmail.com>
2023-12-16 11:35:52 -0600

    se ajustan componentes, rutas, configuraciones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import { SectionPageError } from "./pages/SectionPageError/SectionPageError";
 import { Home } from "./pages/Home";
 /* import { Login } from "./pages/Login/Login";
@@ -7,7 +6,7 @@ import { Registro } from "./pages/Registro/Registro"; */
 import { TeamMembers } from "./pages/TeamMembers/TeamMembers";
 import { Dashboard } from "./pages/Dashboard/Dashboard";
 import { DashboardCardClicked } from "./pages/DashboardCardClicked/DashboardCardClicked";
-
+import "./styles/global.css";
 function App() {
   return (
     <>
